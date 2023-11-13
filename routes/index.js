34e7a9bd6b44621d6e9024f9140fdc7ba552d6d4
rapidemporium <150315341+rapidemporium@users.noreply.github.com>
2023-11-13 10:23:24 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -72,6 +72,29 @@ router.get('/test', function(req, res, next){
   res.render('test');
 })
 
+router.get('/products', async (req, res) => {
+  try {
+    const category_id = 1223; // Replace with the desired category ID
+
+    // Make a request to the MooGold API to list products for the specified category
+    const response = await axios.post('https://moogold.com/wp-json/v1/api/product/list_product', {
+      path: 'product/list_product',
+      category_id,
+    });
+
+    // Extract relevant product information from the API response
+    const products = response.data.map(product => ({
+      ID: product.ID,
+      post_title: product.post_title,
+    }));
+
+    res.json(products);
+  } catch (error) {
+    console.error('Error fetching products:', error.message);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
 
 // router.use("/payment", async (req, res, next) => {
 
