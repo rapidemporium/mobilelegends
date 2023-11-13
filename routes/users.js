b71c72021e9fa16f7015c12a353cb4a2aaf800c0
rapidemporium <150315341+rapidemporium@users.noreply.github.com>
2023-11-13 20:55:26 +0530

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,25 @@
-var express = require('express');
-var router = express.Router();
+var mongoose = require("mongoose");
+var plm = require("passport-local-mongoose");
+var findOrCreate = require("mongoose-findorcreate"); // Add this line
+require('dotenv').config();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+const DB = `mongodb+srv://rapidemporium:${process.env.MONGO_PASSWORD}@cluster0.xhnizbn.mongodb.net/?retryWrites=true&w=majority`;
+
+try {
+  mongoose.connect(DB).then(function() {
+    console.log("Database is now connected!");
+  });
+} catch (err) {
+  console.error("Connection error:", err);
+}
+
+var userSchema = new mongoose.Schema({
+  firstname: String,
+  lastname: String,
+  email: String,
+  password: String
 });
 
-module.exports = router;
+userSchema.plugin(plm, { usernameField: "email" });
+userSchema.plugin(findOrCreate); // Use the findOrCreate plugin
+module.exports = mongoose.model("user", userSchema);
